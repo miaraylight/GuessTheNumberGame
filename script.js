246591f5a26ccd7f70dfe9832904398b6fc836c2
Mia Taitelieva <mtaitelieva@icstars.org>
2025-01-06 14:12:19 -0600

    main logic completed

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const guessForm = document.getElementById("guessForm")
-
+const playground = document.getElementById("playground")
+const note = document.querySelector(".note");
+const attempts = document.querySelector(".attempts");
 let numberOfAttempts = 5;
 
-
+let arrayOfOptions = [];
+attempts.textContent = `Attempts: ${numberOfAttempts}`;
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -11,27 +13,57 @@ function randomNumber(min, max) {
 let compGuess = randomNumber(1, 100);
 console.log(compGuess);
 
+
+
+
+function optionsGenerator(ammountOfOptions, array) {
+    for (let index = 0; index < ammountOfOptions; index++) {
+        let randomInt = randomNumber(1, 100)
+        array.push(randomInt)
+        
+    }
+    array.push(compGuess)
+    return array
+}
+
+
+optionsGenerator(5, arrayOfOptions)
+
 let numberOfAttemptsCounter = 0
 
 
-function handleSubmit(event) {
-    let userNumber = event.target.elements[0].value
-    numberOfAttemptsCounter++
-    console.log(numberOfAttemptsCounter);
-    
-    console.log(userNumber);
-    if (numberOfAttemptsCounter < numberOfAttempts  && userNumber == compGuess) {
-        console.log("You won!")
-    
-    } else if (numberOfAttemptsCounter < numberOfAttempts && userNumber !== compGuess){
-       
-        userNumber < compGuess ? console.log("too low"): console.log("too high");
-        
-        
-    } else if (numberOfAttempts === 5){
-        console.log("Attempts run out")}
-    event.preventDefault();
+
+
+function handleOnClick(e) {
+  
+  numberOfAttemptsCounter++;
+  attempts.textContent = `Attempts: ${numberOfAttempts - numberOfAttemptsCounter}`; 
+  let userNumber = parseInt(e.target.textContent);
+  console.log(typeof(userNumber));
+  console.log(note);
+  
+  if (numberOfAttemptsCounter < numberOfAttempts && userNumber == compGuess) {
+    note.textContent = "Victory!"
+    console.log("You won!");
+  } else if (
+    numberOfAttemptsCounter < numberOfAttempts &&
+    userNumber !== compGuess
+  ) {
+    userNumber < compGuess ? note.textContent = "Too low!" : note.textContent = "Too high!";
+  } else if (numberOfAttempts === 5) {
+    note.textContent = "Attemps run out"
+    console.log("Attempts run out");
+  }
+  e.preventDefault();
+}
+
+for (let index = 0; index < arrayOfOptions.length; index++) {
+    let option = document.createElement("button")
+    option.textContent = arrayOfOptions[index]
+    option.classList.add(".optionBtn");
+    option.addEventListener("click", handleOnClick)
+    playground.appendChild(option)
     
 }
 
-guessForm.addEventListener("submit", handleSubmit);
+
